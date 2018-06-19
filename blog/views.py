233a75a6bf94050812e0aa4b945a0f8819a4f70c
yunyeogyeong <dbsdurud456@naver.com>
2018-06-19 17:01:55 +0900

    어플리케이션 확장하기 까지

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+#    me = User.objects.get(username='admin')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
